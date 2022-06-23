f2457e800eb684a697ff748509db78ee213e6600
Mert Arinci <mertarinci96@gmail.com>
2022-06-24 00:29:23 +0300

    .

--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {addNewPost, getAllPosts} = require("../controllers/posts")
+const {addNewPost, getAllPosts} = require("../controllers/posts");
 
 
 
